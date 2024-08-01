f40f9c6f696371d4857aae52ce18443518e4c0d0
usero4 <knklnlkn@gmail.com>
2024-08-01 11:46:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ genai.configure(api_key=API_KEY)
 generation_config = {
     "temperature": 1,
     "top_p": 0.95,
-    "top_k": 10,
+    "top_k": 64,
     "max_output_tokens": 8192,
     "response_mime_type": "text/plain",
 }
